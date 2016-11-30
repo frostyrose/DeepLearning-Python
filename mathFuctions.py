2cd3f86d2290c11bdd40bf2cbd79929fad0b2175
nadiaz011 <nickdiaz3@msn.com>
2016-11-30 12:29:47 -0500

    Added to ChiSquared
    Only a few lines, but I fleshed it out so it should actually do
    something - may or may not be quite what we want just yet.

--- mathFuctions.py
+++ mathFuctions.py
@@ -1,4 +1,4 @@
-#import Scipy.stats as Stats
+import scipy.stats as Stats
 import DataUtility as du
 import urllib3
 
@@ -14,9 +14,16 @@ def readInFile(dataFile):
     f.close()
     resp.release_conn()
 
-    # data,headers = du.loadCSVwithHeaders(dataFile)
-    return
+    return "resources/" + dataFile #return location of locally saved file
 
 def chiSquaredTest(dataFile):
-    f_obs = readInFile(dataFile)
-    return #Stats.chisquare(f_obs)
+    filename = readInFile(dataFile) #where datafile will be the location of the data on the external machine
+    f_obs, headers = du.loadCSVwithHeaders(filename)
+
+    result_statistic, pvals = Stats.chisquare(f_obs)
+
+    newFileName = "Placeholder" #we can come up with a more formal scheme later
+    du.writetoCSV(result_statistic, newFileName, headers)
+
+
+    return newFileName #passing the file name back up so that the main Flask code can handle sending the file back to Java
