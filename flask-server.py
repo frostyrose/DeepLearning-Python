f52f33024025c0d0685e456ac749a434eba5293c
Cameron Jones <ctjones@wpi.edu>
2017-04-11 00:25:23 -0400

    Adding Anthony's Code to Flask

--- flask-server.py
+++ flask-server.py
@@ -1,30 +1,49 @@
 #import urllib3
 import RNN
+import ApplyDetector
 from mathFunctions import *
 from flask import Flask
 
 app = Flask(__name__)
+hostname = "localhost"
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def root_menu():
+    return 'Welcome to Flask - Server is Running\n----------Settings----------\n  Hostname: ' + hostname
 
 
 @app.route('/TEST/<pickleFile>/<dataFile>')
 def runTest(pickleFile, dataFile):
-    dataFile2 = "/home/ubuntu/DeepLearning-Python/Dataset/" + dataFile
+    local_file = "/home/ubuntu/DeepLearning-Python/Dataset/" + dataFile
     pickleFile2 = pickleFile + ".pickle"
     print dataFile2
     print pickleFile
     if(pickleFile == "RNN"):
-        RNN.evaluate_model(dataFile2, pickleFile2, 200, 1, 20, step_size=0.005, balance_model=False, scale_output=True, variant="GRU")
+        ApplyDetector.apply_model(pickleFile2, local_file, "/home/ali/Results/"+filename)
+        #RNN.evaluate_model(dataFile2, pickleFile2, 200, 1, 20, step_size=0.005, balance_model=False, scale_output=True, variant="GRU")
     return ("Testing " + dataFile + " on the " + pickleFile + " model.")
 
+
 @app.route('/TRAIN/<testType>/<filename>')
-def runTraining(testType,filename):
+def runTraining(testType, filename):
+    local_file = downloadFileFromJavaFTPServer(filename)
     if(testType == "RNN"):
-        RNN.train_and_evaluate_model(200, 1, 20, step_size=0.005, balance_model=False, scale_output=True, variant="GRU")
+        model = ApplyDetector.train_and_save_model("RNN.pickle", local_file, "/home/ali/Results/"+filename)
     return "Training on %s" % filename
+
+
+@app.route('/UPDATE/<variableName>/<value>')
+def updateVariableWithValue(variableName, value):
+    s1 = ""
+    s2 = ""
+    if(variableName == "HOSTNAME"):
+        s1 = "hostname"
+        s2 = str(value)
+        hostname = value
+    else:
+        return ("No Valid Variable Selected")
+    return ("Variable associated with " + s1 + " updated to the value" + s2)
+
 
 @app.route('/MATH/<testType>/<dataFile>')
 def runMath(testType, dataFile):
@@ -40,7 +59,6 @@ def runMath(testType, dataFile):
         ttest(dataFile, 2)
     if str(testType) == "ttestRel":
         ttest(dataFile, 3)
-
     else:
         return ("No Valid Test Selected")
     return ("Running " + testType + " on the " + dataFile + " data.")
